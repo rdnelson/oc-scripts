8eb200b22382a862a80e7053f5ca642f84ad7fd0
Robert Nelson <git@rnelson.ca>
2024-06-30 14:50:16 -0400

    Change table syntax.

--- nc-mon.lua
+++ nc-mon.lua
@@ -1,9 +1,9 @@
 local component = require("component")
 local device_names = {
-    "9921336b-0214-402a-85be-f03eaa864f13" = "Energy Bank 1",
-    "fccf9042-b8a6-45d6-850c-4b2502a32081" = "Energy Bank 2",
-    "b54ccde6-9895-4992-a7a8-0988cc979b28" = "Energy Bank 3",
-    "ef28db82-3c9d-4d23-a70b-a30ac65e47d5" = "Energy Bank 4",
+    ["9921336b-0214-402a-85be-f03eaa864f13"] = "Energy Bank 1",
+    ["fccf9042-b8a6-45d6-850c-4b2502a32081"] = "Energy Bank 2",
+    ["b54ccde6-9895-4992-a7a8-0988cc979b28"] = "Energy Bank 3",
+    ["ef28db82-3c9d-4d23-a70b-a30ac65e47d5"] = "Energy Bank 4"
 }
 
 function dev_by_type(type)
